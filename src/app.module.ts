b3df64c6c3a91ffd9185bc8eb224a60bacf073ef
dieselhuang <dieselhuang@tencent.com>
2022-12-14 17:17:30 +0800

    feat: 安装 TypeORM 并配置数据库信息

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { HcModule } from './hc/hc.module';
+// 引入 typeORM 模块
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [HcModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'bologer',
+      password: 'weiwei10',
+      database: 'nft-info',
+      // 重试链接数据库间隔
+      retryDelay: 500,
+      // 重试次数
+      retryAttempts: 10,
+    }),
+    HcModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/hc/hc.controller.ts
+++ src/hc/hc.controller.ts
@@ -1,11 +1,28 @@
-import { Controller, Get, Post, Request, Query, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Query, Body, Param } from '@nestjs/common';
 import { HcService } from './hc.service';
+import openai from 'openai';
+const { Configuration, OpenAIApi } = require("openai");
 
-@Controller('hc')
+@Controller('api/hc')
 // 下面写方法
 export class HcController {
     constructor(private hcService: HcService) {
         // this.hcService = HcService
+    }
+
+    @Get('chat')
+    async chat() {
+        const apiKey = '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'
+        const configuration = new Configuration({
+            apiKey,
+        });
+        const openai = new OpenAIApi(configuration);
+        console.log(configuration, openai)
+        const completion = await openai.createCompletion({
+            model: "text-davinci-002",
+            prompt: "夸赞一个人很帅，隐晦一些",
+        });
+        console.log(completion.data.choices[0].text);
     }
 
     @Get('list')
@@ -25,7 +42,7 @@ export class HcController {
     //     let id: number = parseInt(query.id)
     //     return this.hcService.getItemDetail(id)
     // }
-    
+
     // 动态路由方式
     @Get('getItemDetail/:id/:name')
     getItemDetail(@Param() params): any {
